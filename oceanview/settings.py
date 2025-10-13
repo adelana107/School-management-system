7c9febfe0262238510b8454699a3bbea9ca81ad4
Adelana107 <adelana787898@gmail.com>
2025-10-13 22:26:40 +0100

    Fix allowed hosts for Render

--- oceanview/settings.py
+++ oceanview/settings.py
@@ -17,7 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-dev-key")
 DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ['oceanview-university.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # -------------------------
